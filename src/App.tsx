1437933604cfbf1b19104a620f7e8f5c325ad680
Khurshid <mohammad.k1@infoedge.com>
2024-08-21 00:07:36 +0530

    hsuky pre-commit prettier test

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,15 @@
+import './style.css';
+import Logo from './images/React-logo.png';
+import SvgLogo from './images/react.svg';
+
 const App = () => {
-    return <h1>Hello</h1>
+	return (
+		<>
+			<h1>Hello</h1>
+			<img src={Logo} alt="react-logo" width={300} height={200} />
+			<img src={SvgLogo} alt="react-svg-logo" width={300} height={200} />
+		</>
+	);
 };
 
 export default App;
